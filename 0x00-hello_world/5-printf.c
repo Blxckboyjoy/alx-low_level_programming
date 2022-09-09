e5a8e890d5a0f113cf6d889e7a72f65407386342
Blxckboyjoy <111021468+Blxckboyjoy@users.noreply.github.com>
2022-09-09 15:27:40 +0200

    fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main-prints a string
+ * description ;uses printf function
+ * return:0
+ */
+
+int main(void)
+{
+    printf("%s", " with proper grammar, but the outcome is a piece of art,\n");
+    return(0);
+}
