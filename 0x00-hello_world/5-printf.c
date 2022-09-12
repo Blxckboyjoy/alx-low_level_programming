559487a611b76536143b372284b1f699e48968ac
Natangwe <natangwetj@gmail.com>
2022-09-12 05:46:12 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,15 +2,15 @@
 
 /**
  * main - entry point
- * description:write program to print 
+ * description:'write program to print' 
  * return - always zero
  */
 
 int main(void)
 {
-  
+
 printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
-  
+
 }
