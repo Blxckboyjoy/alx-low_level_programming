5d7ef6979526a462bcfa4f0bc4f1d5f0ebdb82f5
Natangwe <natangwetj@gmail.com>
2022-09-19 11:58:59 +0200

    update to files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks code 
+ * return - 0
+ */
+int main(void);
+{
+    char *str;
+
+    str = "I do not fear compuers.I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
+} 
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ * return -0
+ */
+int main(void)
+{
+    char*str;
+
+    str = "0123456789";
+    puts2(str);
+    }
+            if char*str = odd;
+    {
+    printf(lenght_of_the_string - 1)\2;
+    }
+
+    return(0);
+}
