3b5889cbfd7255b8dd404160a0fd757b5318a9e6
Natangwe <natangwetj@gmail.com>
2022-09-19 16:22:11 +0200

    commemt fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 
 /**
  * swap_int - changes two values
- * a -  pointer first integer
- * b - pointer second integer
+ * 'a' -  pointer first integer
+ * 'b' - pointer second integer
  * return - none
  */
 void swap_int(int *a, int *b)
