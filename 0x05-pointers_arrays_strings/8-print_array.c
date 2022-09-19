8c64bacc371f929492182900f90de50862d9fc1f
Natangwe <natangwetj@gmail.com>
2022-09-19 18:02:23 +0200

    edit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,13 @@
  */
 void print_array(int *a, int n)
 {
-int i;
-if (n != 0 && n != 1)
-for (i = 0; i < n; i++)
-{
-if (i != (n - 1))
-printf("%d, ", a[i]);
-else if (i == (n - 1))
-printf("%d", a[i]);
-}
+	int i;
+
+	if (n != 0 && n != 1)
+		for (i = 0; i < n; i++)
+		{
+			if (i != (n - 1))
+				printf("%d, ", a[i]);
+			else if (i == (n - 1))
+				printf("%d", a[i]);
+		}
