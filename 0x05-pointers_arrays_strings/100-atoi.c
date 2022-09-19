985208e3b84f005502041ced3ba37507e4863138
Natangwe <natangwetj@gmail.com>
2022-09-19 14:55:00 +0200

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,28 +14,29 @@ int len = 0;
 int f = 0;
 int digit = 0;
 
-while 
+while
 (s[len] != '\0')len++;
-while 
+while
 (i < len && f == 0)
 {
-if (s[i] == '-') ++d;
+if
+(s[i] == '-') ++d;
 
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
 if (d % 2)
 digit = -digit;
-n = n *10 + digit;
+n = n * 10 + digit;
 f = 1;
-if (s[i +1] < '0' || s[i + 1] > '9')
+if (s[i + 1] < '0' || s[i + 1] > '9')
 
-break;;
+break;
 f = 0;
 }
 i++;
 }
-if (f ==0)
+if (f == 0)
 return (0);
 
 return (n);
