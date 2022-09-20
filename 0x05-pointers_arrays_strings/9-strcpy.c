214cdd40e2c2bc3051c81123a8b6934ea21650b7
Natangwe <natangwetj@gmail.com>
2022-09-20 08:29:45 +0200

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strcpy - a function copies the string pointed  to by src ,
+ * _strcpy: a function copies the string pointed  to by src ,
  * including the terminating null byte(\0)'
  * to the buffer pointed to by dest.
  * @dest: array returned by pointer
