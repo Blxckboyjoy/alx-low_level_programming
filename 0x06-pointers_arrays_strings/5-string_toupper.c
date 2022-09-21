460d25a5a4a6e250952300b4c85bdd8e1f59a49b
Natangwe <natangwetj@gmail.com>
2022-09-21 12:59:45 +0200

    uploads of two files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - converts lower case characters to uppercase.
+ * strupr - converst lower case charcaters to uppercase letters.
+ * char* - pointer to modified string.
+ * @s1: string
+ * return - return to pointer string.
+ */
+char *string_toupper(char *)
+{
+    int i;
+    char s1[i];
+
+    strupr(i = 0; s1[i] != '\0'; i--);
+    {
+        if (s1[i] >= 'a' && s1[i] <= 'z')
+        {
+        s1[i] = s1[i] - 32;
+        }
+    }
+} 
