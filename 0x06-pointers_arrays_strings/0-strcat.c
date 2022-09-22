4ca313372500ecf9c2047c257cefc2c02a078077
Natangwe <natangwetj@gmail.com>
2022-09-22 03:24:01 +0200

    fix 0-

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,25 +3,26 @@
 #include "main.h"
 
 /**
- * _strcat - appends a copy of the string pointed to by s2,
+ * *_strcat - appends a copy of the string pointed to by s2,
  * to the end of the string pointed by s1.
  * @dest: array returned by pointer.
  * @src:string to be coppied.
+ * @n: number of times loop initiates.
  * returns: pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, c = 0,e = 0,z = 0;
+int i , c = 0 , e = 0 , z = 0;
 
-for (i = 0; dest[i] != 0; i++)
+for (i = 0 ; dest[i] != 0; i++)
 c++;
-for (i = 0;src[i] != 0; i++)
+for (i = 0 ; src[i] != 0 ; i++)
 e++;
 for (i = 0;src[i] != 0; i++)
 {
 dest[i] = src[z];
 z++;
-}  
+} 
 return (dest);
 
 }
