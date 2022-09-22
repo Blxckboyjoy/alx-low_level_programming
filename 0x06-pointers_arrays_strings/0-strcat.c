8065f3b0b924259160a8b247e455ff1df12c5b45
Natangwe <natangwetj@gmail.com>
2022-09-22 03:31:34 +0200

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * *_strcat - function that cocantenates two strings.
+ * _strcat - function that cocantenates two strings.
  * @dest: array returned by pointer.
  * @src:string to be coppied.
  * @n: number of times loop initiates.
