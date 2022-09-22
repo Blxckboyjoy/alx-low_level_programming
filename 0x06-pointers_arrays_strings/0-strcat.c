779366295c2557ac63093ad786b85eaf980c5017
Natangwe <natangwetj@gmail.com>
2022-09-22 03:19:49 +0200

    whitespace removal

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, c=0,e=0,z=0;
+int i, c = 0,e = 0,z = 0;
 
-for(i = 0; dest[i] != 0; i++)
+for (i = 0; dest[i] != 0; i++)
 c++;
-for(i = 0;src[i] != 0; i++)
+for (i = 0;src[i] != 0; i++)
 e++;
-for(i = 0;src[i] != 0; i++)
+for (i = 0;src[i] != 0; i++)
 {
-    dest[i] = src[z];
-    z++;
-}       
-return (dest); 
+dest[i] = src[z];
+z++;
+}  
+return (dest);
 
 }
