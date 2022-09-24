8b9310a9e6588b419d7197b79b0fcd046e8fe976
Natangwe <natangwetj@gmail.com>
2022-09-24 12:05:21 +0200

    fix  0-

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j, len1, len2, l2;
-char dest;
-char src= j;
-len1 = strlen(dest);
-len2= strlen(src);
-for (i= 0; i <= l2; i++)
-{
-dest[len1+ i]= src[i];
-}
-printf("%s\n",dest);
-puts(src);
+int i, dest_len; 
+dest_len = strlen(dest);
+
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+
+return dest;
 }
