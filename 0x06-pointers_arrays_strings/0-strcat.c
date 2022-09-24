7f6b06763dd74729706ca0f083452e8d82c456f8
Natangwe <natangwetj@gmail.com>
2022-09-24 12:41:59 +0200

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,14 @@
  * @dest: pointer of the cocatenated string.
  * @src:string to be coppied.
  * @n: number of times loop initiates.
+ * Return: none.
  */
-char *_strncat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-int i, j;
-i = 0;
-j = 0;
-
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+int i;
+char dest;
+char src;
+strncat(dest, src, i);
+printf("%s\n", dest);
+puts(src);
 }
