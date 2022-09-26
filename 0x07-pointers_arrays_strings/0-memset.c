fe4dd6fc89aeec647a2643c28e7269bfeb0dfa54
Natangwe <natangwetj@gmail.com>
2022-09-26 23:33:00 +0200

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,11 +3,9 @@
 #include "main.h"
 
 /**
- * memset - fill memory with a constant byte.
+ * _memset - fill memory with a constant byte.
  * @*s: a pointer to the block of memory to fill.
- * @b: the value to be set. The value is passed as an,
- * int but the function fills the block of memory,
- * using the unsigned char conversion of this value.
+ * @b: constant byte to fill.
  * @n: This is the number of bytes to be set to the value.
  * Return - returns a pointer to the memory area str.
  */
