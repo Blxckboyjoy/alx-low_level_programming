e272b48f8454dba3cc4071e7953695a4c1b91e36
Natangwe <natangwetj@gmail.com>
2022-09-28 14:58:57 +0200

    upload task 0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; i < 1 ; i++)
-{
-if (s[i] == c)
-return ((s + i));
+	int i;
 
-if (s[i] == 0)
-break;
-}
-return (NULL);
+	for (i = 0; 1 ; i++)
+	{
+		if (s[i] == c)
+			return ((s + i));
+		if (s[i] == 0)
+			break;
+	}
+	return (NULL);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,23 +8,20 @@
  * @accept: string containing the characters to match.
  * Returns - nothing.
  */
-
- unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-int i;
-int j;
-int e = 0;
+	unsigned int i, j;
+	unsigned int c = 0;
 
-for (i = 0; i != s[i] ; i++)
-{
-if (s[i] = ' ')
-break;
-for (j = 0; accept[j] != j; j++)
-{
-if (s[i] = accept[j])
-j++;
-}
-
-}
-return (e);
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == ' ')
+			break;
+		for (j = 0; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+				c++;
+		}
+	}
+	return (c);
 }
