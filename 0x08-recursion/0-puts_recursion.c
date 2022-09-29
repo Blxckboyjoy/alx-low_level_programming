e06320b0e41304369585ba5e1782bd1aac5e2018
Natangwe <natangwetj@gmail.com>
2022-09-29 12:15:43 +0200

    0x08 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+if 
+(s == 0) return  ;
+else 
+{
+_puts_recursion(s-1);
+puts(s);
+}
+void main (void);
+{
+int i;
+char s = i;
+_puts_recursion(i);
+}
+
+}
