22b1f5d4833a87a9381fdd1d55b2a3ca89cd3b07
Natangwe <natangwetj@gmail.com>
2022-09-29 14:07:57 +0200

    08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,21 @@
 #include "main.h"
 
 /**
- *
+ * _puts_recursion - function calls a copy of itself,
+ * to work on a smaller problem.
+ * 
  *
  */
 void _puts_recursion(char *s)
 {
-if 
-(s == 0) return  ;
-else 
+if
+(s == 0) return;
+else
 {
-_puts_recursion(s-1);
+_puts_recursion(s - 1);
 puts(s);
 }
-void main (void);
+void main(void);
 {
 int i;
 char s = i;
