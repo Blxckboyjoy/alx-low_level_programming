9fa566c36dc81b5680f47ebf50cd33724a7b4ec0
Natangwe <natangwetj@gmail.com>
2022-10-03 19:46:55 +0200

    betty fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,7 @@
  * main - program that prints name
  * @argc: number of command-line arguments
  * @argv: array of strings containing the command-line arguments
+ * Returns - 0
  */
 int main(int argc, char *argv[])
 {
