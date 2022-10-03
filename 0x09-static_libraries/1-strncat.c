cf671bfb4cdc55a092ad9d69d9e267cfac103849
Natangwe <natangwetj@gmail.com>
2022-10-03 15:13:11 +0200

    q1

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * _strncat - appends the string pointed,
- * to by src to the end of the string'
- * pointed by dest up to n characters long.
- * strcpy - copies the string pointed,
- * (including null character) to the destiation.
- * strlen - find lenght of string.
- * @dest: pointer to the destination array
- * @src: string to be appended.
- * @n:the maximum number of characters to,
- * be appended.
- * Return: pointer to the resulting string dest.
- */
+* _strncat - a function that cocantenates two strings
+* @dest: the pointer to the cocantenated string
+* @src: the string to be appended
+* @n: the number of bytes from src to be appended
+* Return: returns a pointer to the string dest
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = strlen(dest);
-int i;
+	int i, c = 0, j = 0;
 
-for (i = 0 ; i < n && src[i] != '\0' ; i++)
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
-
-return (dest);
+	for (i = 0; dest[i] != 0; i++)
+		c++;
+	for (i = c; src[j] != 0 && j < n; i++)
+	{
+		if (i == 98)
+			break;
+		dest[i] = src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
