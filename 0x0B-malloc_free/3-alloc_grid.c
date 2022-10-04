c86453652d884f5fec7961d6294359b614b8e08c
Natangwe <natangwetj@gmail.com>
2022-10-04 11:23:01 +0200

    n

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ len = sizeof(int *) *hight + sizeof(int) *width *hight;
 arr = (int **) malloc(len);
 if (arr == NULL)
 return (NULL);
-ptr = (int *) *(arr + hight);
+ptr = (int *) *(arr +hight);
 
 for (i = 0; i < hight; i++)
 {
