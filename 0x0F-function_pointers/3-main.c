9400bb9f2b75988c611fbd9393a58c4da6d7a204
Natangwe <natangwetj@gmail.com>
2022-10-13 12:55:37 +0200

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,34 +12,34 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*ptr)(int, int);
+int num1, num2, result;
+int (*ptr)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
-		|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
-		{
-			printf("Error\n");
-			exit(100);
-		}
+if (strlen(argv[2]) == 1 && (argv[2][0] == '+' || argv[2][0] == '-'
+|| argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
+{
+if (atoi(argv[3]) == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
 
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-		ptr = get_op_func(argv[2]);
-		result = ptr(num1, num2);
+ptr = get_op_func(argv[2]);
+result = ptr(num1, num2);
 
-		printf("%d\n", result);
+printf("%d\n", result);
 
-		return (0);
-	}
-	printf("Error\n");
-	exit(99);
+return (0);
+}
+printf("Error\n");
+exit(99);
 }
